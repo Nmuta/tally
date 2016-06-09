0323f792967f97ae79b5c9f1ad454df84e6908de
Alex <amurphy345@gmail.com>
2016-06-09 09:49:33 -0600

    7 lines and done

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+$('[name="button"]').click(function(){
+  $('#total').html($('.selectable').val().map((val) => {
+    return parseFloat(val.replace(/[^\d.-]/g, ''))
+  }).reduce((prev, curv) => {
+    return prev + curv
+  }))
+})
