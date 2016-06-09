210b33e7b16d20f079a62a0334f79199e294b590
Michael Coulter <msc2086@gmail.com>
2016-06-09 10:23:30 -0600

    Coulter

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-
+$(document).ready(function(){
+  var click=document.getElementsByTagName('button')[0];
+  var opt=document.getElementsByTagName("option");
+  var tot=document.getElementById('total')
+  console.log(opt);
+  var arr =[]
+  click.addEventListener("click", function(){
+    for (var i = 0; i < opt.length; i++) {
+      if(opt[i].selected){
+        arr.push(Number(((opt[i].value).split(": "))[1]));
+      };
+    }
+    console.log(arr);
+    var x = arr.reduce(function(prev,curr){
+        return prev+curr
+      })
+    console.log(x);
+    var thing = document.createElement("div")
+    thing.innerHTML= "$"+x.toFixed(2)
+    tot.appendChild(thing)
+  });
+  })
