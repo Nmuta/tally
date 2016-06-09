78e5bd2cb638c8463a86190650455118b1e70be6
Tom <tomhendersonfl@gmail.com>
2016-06-09 10:37:36 -0600

    Completed Exercise

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+document.getElementsByName("button")[0].addEventListener("click",function() {doPrices()});
 
+function doPrices() {
+  var values=getSelections()
+  var result=0
+  el=document.getElementById("total")
+  if (values.length === 0) {
+    result = 0
+  } else {
+    result = values.reduce(function(total, next) {
+    return Number(total) + Number(next);
+    })
+  }
+  el.innerHTML = `The sum is ${result.toFixed(2)}`
+}
+function getSelections() {
+  var prices = document.getElementsByName("toys")[0]
+  var values = []
+  for (var i = 0; i < prices.options.length; i++) {
+    if (prices.options[i].selected) {
+      var tempArray = prices.options[i].value.split(" ")
+      values.push(Number(tempArray[tempArray.length - 1]))
+    }
+  }
+  return values
+}
