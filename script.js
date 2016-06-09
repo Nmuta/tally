84a295b50968e070a7e92e7a87a320f37b79207e
Julie DeMasters <juliekdemasters@gmail.com>
2016-06-09 10:16:37 -0600

    final working document

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+var toys = document.getElementsByTagName("option");
+var button = document.getElementById('button');
+var totalBox = document.getElementById('total')
+var total = document.createElement('p');
 
+button.addEventListener('click', function(){
+    total.innerHTML = "";
+    var counter = 0;
+    for (var i = 0; i < toys.length; i++) {
+      if(i===0 || i===5 ){
+      }
+      else if (toys[i].selected){
+        counter += Number(toys[i].value.split(" ")[3]);
+        console.log(counter)
+      }
+    }
+    total.innerHTML = "$" + counter;
+    totalBox.appendChild(total)
+
+})
