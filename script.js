af96e1b051b706e515d321dbc09fac4690595df3
Scott Wilkins <scottwilkins@MacBook-Air.local>
2016-06-09 10:14:35 -0600

    add solution

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+$(document).ready(function(){
+  $('button').on('click', function(){
+    var $items = $(".selectable").val().map(a=>{
+                 var splits = a.split(" : "); return splits[1]}).
+                 reduce((a,b)=>+a + +b)
 
+   $(".displayBox").html(`$${$items.toFixed(2)}`)
+
+  })
+})
